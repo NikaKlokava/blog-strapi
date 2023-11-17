d4398b003459f024fa791d052b0b68f285832be1
NikaKlokava <veronika.kazakevich1997@gmail.com>
2023-11-17 12:11:45 +0200

    feat: create header and footer components

---
+++ frontend/app/components/footer/Footer.tsx
@@ -0,0 +1,23 @@
+import styles from "./Footer.module.css";
+import classes from "classnames";
+import { links } from "@/app/utils/utils";
+
+export const Footer = () => {
+  return (
+    <div className={styles.footer_container}>
+      <p className="text-sm">Created by Veranika Klokava</p>
+      <p className="text-sm">2023</p>
+      <div className={styles.links_container}>
+        {links.map((link) => {
+          return (
+            <a
+              key={link.name}
+              className={classes(styles[`${link.name}`], styles.icon)}
+              href={`${link.href}`}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
