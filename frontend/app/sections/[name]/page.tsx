c4bc6f9a7e0527618c7669b8b2566ac4f6cd85a8
NikaKlokava <veronika.kazakevich1997@gmail.com>
2023-11-17 17:38:45 +0200

    feat: style section pages

--- frontend/app/sections/[name]/page.tsx
+++ frontend/app/sections/[name]/page.tsx
@@ -1,9 +1,41 @@
+import Image from "next/image";
+import styles from "./styles.module.css";
+import { sectionsData } from "@/mocks/mocks";
+
 type Props = {
   params: { name: string };
 };
 
 const SectionPage = ({ params }: Props) => {
-  return <div className=" text-center">{params.name} Page</div>;
+  const section = sectionsData.find((section) => section.name === params.name);
+
+  return (
+    <div className={styles.section_container}>
+      <div className={styles.section_logo}>
+        <div className={styles.logo}>
+          <Image
+            src={section?.logo}
+            alt={section?.name!}
+            className={styles.logo_img}
+          />
+        </div>
+      </div>
+      <h1>{params.name.toUpperCase()}</h1>
+      <div className={styles.grid_container}>
+        {section?.data.map((item, index) => {
+          return (
+            <Image
+              src={item.photo}
+              alt={"item.descr"}
+              key={index}
+              width={350}
+              height={450}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default SectionPage;
--- frontend/mocks/mocks.ts
+++ frontend/mocks/mocks.ts
@@ -1,3 +1,10 @@
+import image1 from "../public/test_img1.jpg";
+import image2 from "../public/test_img2.jpg";
+import image3 from "../public/test_img3.jpg";
+import logo1 from "../public/logo1.svg";
+import logo2 from "../public/logo2.svg";
+import logo3 from "../public/logo3.svg";
+
 export const links = [
   {
     name: "linkedin",
@@ -36,5 +43,34 @@ export const navbarItems = [
   {
     name: "ABOUT",
     path: "/about",
+  },
+];
+
+export const sectionsData = [
+  {
+    name: "travel",
+    logo: logo1,
+    data: Array(10).fill({
+      descr: "travel_img",
+      photo: image1,
+      
+    }),
+  },
+  {
+    name: "life",
+    logo: logo2,
+    data: Array(18).fill({
+      descr: "travel_img",
+      photo: image2,
+      
+    }),
+  },
+  {
+    name: "styles",
+    logo: logo3,
+    data: Array(15).fill({
+      descr: "travel_img",
+      photo: image3,
+    }),
   },
 ];
