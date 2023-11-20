67195882c38d0a7f7f2e3c7caf67aa44dc1275d8
NikaKlokava <veronika.kazakevich1997@gmail.com>
2023-11-20 14:17:10 +0200

    feat: update join the blog component

--- frontend/app/components/join-the-blog/JoinTheBlog.tsx
+++ frontend/app/components/join-the-blog/JoinTheBlog.tsx
@@ -2,12 +2,15 @@
 
 import styles from "./JoinTheBlog.module.css";
 import { Formik, Form, Field } from "formik";
+import { initialFormValue, validationSchema } from "@/app/utils/utils";
 
 export const JoinTheBlog = () => {
   return (
     <Formik
-      initialValues={{ data: "" }}
+      initialValues={initialFormValue}
+      validationSchema={validationSchema}
       onSubmit={(values) => {
+        alert("Success");
         console.log(values);
       }}
     >
@@ -20,21 +23,30 @@ export const JoinTheBlog = () => {
               props.handleSubmit();
             }}
           >
-            <h1>Join the blog!</h1>
-            <Field
-              placeholder="your name"
-              autoComplete="off"
-              type="text"
-              as="input"
-              name={"formikName"}
-            />
-            <Field
-              placeholder="enter your email"
-              autoComplete="off"
-              type="email"
-              as="input"
-              name={"formikName"}
-            />
+            <h1 className={styles.title}>Join the blog!</h1>
+            <div className={styles.input_container}>
+              <Field
+                placeholder="your name"
+                autoComplete="off"
+                type="text"
+                as="input"
+                name={"name"}
+                className={styles.input_field}
+              />
+              <Field
+                placeholder="enter your email"
+                autoComplete="off"
+                type="email"
+                as="input"
+                name={"email"}
+                className={styles.input_field}
+              />
+            </div>
+            {props.errors.name && <div>{props.errors.name}</div>}
+            {props.errors.email && <div>{props.errors.email}</div>}
+            <button className={styles.submit_btn} type="submit">
+              Submit
+            </button>
           </Form>
         </div>
       )}
--- frontend/app/sections/[name]/page.tsx
+++ frontend/app/sections/[name]/page.tsx
@@ -1,39 +1,45 @@
 import Image from "next/image";
 import styles from "./styles.module.css";
 import { sectionsData } from "@/mocks/mocks";
+import { JoinTheBlog } from "@/app/components/join-the-blog/JoinTheBlog";
 
 type Props = {
   params: { name: string };
 };
 
 const SectionPage = ({ params }: Props) => {
-  const section = sectionsData.find((section) => section.name === params.name);
+  const sectionData = sectionsData.find(
+    (section) => section.name === params.name
+  );
 
   return (
-    <div className={styles.section_container}>
-      <div className={styles.section_logo}>
-        <div className={styles.logo}>
-          <Image
-            src={section?.logo}
-            alt={section?.name!}
-            className={styles.logo_img}
-          />
+    <div className={" flex flex-col items-center"}>
+      <div className={styles.section_container}>
+        <div className={styles.section_logo}>
+          <div className={styles.logo}>
+            <Image
+              src={sectionData?.logo}
+              alt={sectionData?.name!}
+              className={styles.logo_img}
+            />
+          </div>
+        </div>
+        <h1>{params.name.toUpperCase()}</h1>
+        <div className={styles.grid_container}>
+          {sectionData?.data.map((item, index) => {
+            return (
+              <Image
+                src={item.photo}
+                alt={"item.descr"}
+                key={index}
+                width={350}
+                height={450}
+              />
+            );
+          })}
         </div>
       </div>
-      <h1>{params.name.toUpperCase()}</h1>
-      <div className={styles.grid_container}>
-        {section?.data.map((item, index) => {
-          return (
-            <Image
-              src={item.photo}
-              alt={"item.descr"}
-              key={index}
-              width={350}
-              height={450}
-            />
-          );
-        })}
-      </div>
+      <JoinTheBlog />
     </div>
   );
 };
