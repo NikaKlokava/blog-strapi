331d89fc7626887ac321079bd917fc45936f137b
NikaKlokava <veronika.kazakevich1997@gmail.com>
2023-11-20 20:05:34 +0200

    feat: create wish list component

---
+++ frontend/app/components/wish-list/WishList.tsx
@@ -0,0 +1,20 @@
+import { wishListData } from "@/mocks/mocks";
+import Image from "next/image";
+import styles from "./WishList.module.css";
+
+export const WishList = () => {
+  return (
+    <div className={styles.wish_list_container}>
+      <div className={styles.title}>Wish List</div>
+      <div className={styles.list_container}>
+        {wishListData.map((item, i) => {
+          return (
+            <a href={item.href} key={i} target="_blank">
+              <Image alt="wish_list_item" src={item.photo} />
+            </a>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
