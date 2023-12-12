35ade438213423ac7ad33e35ca5d1974dfccc5c4
NikaKlokava <veronika.kazakevich1997@gmail.com>
2023-12-12 12:38:40 +0200

    feat: reinstall strapi back

--- backend/src/api/posts/controllers/posts.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * posts controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::posts.posts');
--- backend/src/api/social-links/controllers/social-links.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * social-links controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::social-links.social-links');
