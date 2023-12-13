30aa005e28bb1131cc5bba5e966166ce7270529c
NikaKlokava <veronika.kazakevich1997@gmail.com>
2023-12-13 17:40:46 +0200

    feat: add post req to strapi for saving followers data

--- frontend/app/components/join-the-blog/JoinTheBlog.tsx
+++ frontend/app/components/join-the-blog/JoinTheBlog.tsx
@@ -4,20 +4,37 @@ import styles from "./JoinTheBlog.module.css";
 import { Formik, Form, Field } from "formik";
 import { initialFormValue, validationSchema } from "@/app/utils/utils";
 import classes from "classnames";
+import { useState } from "react";
 
 type Props = {
   extraClass?: string;
 };
 
 export const JoinTheBlog = ({ extraClass }: Props) => {
+  const [success, setSuccess] = useState<boolean>();
+
+  const onSubmit = async (values: { name: string; email: string }) => {
+    try {
+      const data = await fetch("http://localhost:1337" + `/api/followers`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + process.env,
+        },
+        body: JSON.stringify({
+          data: values,
+        }),
+      });
+      data.ok ? setSuccess(true) : setSuccess(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <Formik
       initialValues={initialFormValue}
       validationSchema={validationSchema}
-      onSubmit={(values) => {
-        alert("Success");
-        console.log(values);
-      }}
+      onSubmit={(values) => onSubmit(values)}
     >
       {(props) => (
         <div
@@ -33,6 +50,23 @@ export const JoinTheBlog = ({ extraClass }: Props) => {
               props.handleSubmit();
             }}
           >
+            {success !== undefined && (
+              <div className={styles.status_container}>
+                <div>
+                  {success ? "Success!" : "You have already join my blog!"}
+                </div>
+                <button
+                  className={styles.style_btn}
+                  onClick={() => {
+                    setSuccess(undefined);
+                    props.handleReset();
+                  }}
+                >
+                  Ok
+                </button>
+              </div>
+            )}
+
             <h1 className={styles.title}>Join the blog!</h1>
             <div className={styles.input_container}>
               <Field
@@ -53,7 +87,7 @@ export const JoinTheBlog = ({ extraClass }: Props) => {
               />
             </div>
             {props.errors.name && props.dirty && <div>{props.errors.name}</div>}
-            <button className={styles.submit_btn} type="submit">
+            <button className={styles.style_btn} type="submit">
               Submit
             </button>
           </Form>
