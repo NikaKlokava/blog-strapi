0e31b3b30003b1efb01140b0b500ff45476d9568
NikaKlokava <veronika.kazakevich1997@gmail.com>
2023-12-18 13:05:35 +0200

    fix: fix lifecycle hook

--- backend/src/api/posts/content-types/posts/lifecycles.js
+++ backend/src/api/posts/content-types/posts/lifecycles.js
@@ -1,28 +1,19 @@
 const nodemailer = require("nodemailer");
 
-// const transporter = nodemailer.createTransport({
-//   service: "Gmail",
-//   host: "smtp.gmail.com",
-//   port: 465,
-//   secure: true,
-//   auth: {
-//     user: process.env.SMTP_USER_EMAIL, // env("SMTP_USER_EMAIL")
-//     pass: process.env.SMTP_USER_PASSWORD, //env("SMTP_USER_PASSWORD")
-//   },
-// });
+const transporter = nodemailer.createTransport({
+  service: "Gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.SMTP_USER_EMAIL, // env("SMTP_USER_EMAIL")
+    pass: process.env.SMTP_USER_PASSWORD, //env("SMTP_USER_PASSWORD")
+  },
+});
 
-module.exports = ({ env }) => ({
+module.exports = {
   async afterCreate() {
-    const transporter = nodemailer.createTransport({
-      service: "Gmail",
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
-      auth: {
-        user: env("SMTP_USER_EMAIL"),
-        pass: env("SMTP_USER_PASSWORD"),
-      },
-    });
+    console.log(process.env.SMTP_USER_EMAIL);
 
     const response = strapi.service("api::follower.follower");
     const followers = await response.find();
@@ -30,7 +21,7 @@ module.exports = ({ env }) => ({
     const emails = followers?.results?.reduce((accum, current) => {
       return [...accum, current.email];
     }, []);
-
+    console.log(emails);
     try {
       await transporter.sendMail({
         from: "The Traveler",
@@ -42,4 +33,4 @@ module.exports = ({ env }) => ({
       console.log(err);
     }
   },
-});
+};
