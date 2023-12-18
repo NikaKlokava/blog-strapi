a81f6f2bfc370aa912d298fb8a12682da742c312
NikaKlokava <veronika.kazakevich1997@gmail.com>
2023-12-18 14:10:07 +0200

    fix: update plugin

---
+++ backend/config/plugins.js
@@ -0,0 +1,19 @@
+module.exports = ({ env }) => ({
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: env("SMTP_HOST"),
+        port: env("SMTP_PORT"),
+        auth: {
+          user: env("SMTP_USER_EMAIL"),
+          pass: env("SMTP_USER_PASSWORD"),
+        },
+      },
+      settings: {
+        defaultFrom: env("SMTP_USER_EMAIL"),
+        defaultReplyTo: env("SMTP_USER_EMAIL"),
+      },
+    },
+  },
+});
--- backend/src/api/posts/content-types/posts/lifecycles.js
+++ backend/src/api/posts/content-types/posts/lifecycles.js
@@ -1,31 +1,16 @@
-const nodemailer = require("nodemailer");
-
-const transporter = nodemailer.createTransport({
-  service: "Gmail",
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.SMTP_USER_EMAIL, // env("SMTP_USER_EMAIL")
-    pass: process.env.SMTP_USER_PASSWORD, //env("SMTP_USER_PASSWORD")
-  },
-});
-
 module.exports = {
   async afterCreate() {
-    console.log(process.env.SMTP_USER_EMAIL);
-
     const response = strapi.service("api::follower.follower");
     const followers = await response.find();
 
     const emails = followers?.results?.reduce((accum, current) => {
       return [...accum, current.email];
     }, []);
-    console.log(emails);
+
     try {
-      await transporter.sendMail({
-        from: "The Traveler",
+      await strapi.plugins["email"].services.email.send({
         to: emails,
+        from: "The Traveler",
         subject: `Hello from "Traveler"! I have a new post for you.`,
         text: `Hello! A new post has been released, let's join "The Traveler".`,
       });
